66b8e28f0b44d29721476be835652f30dcc7b467
Hannah Lee <013hannah.a.lee@gmail.com>
2021-05-02 11:24:32 -0500

    updated Product model with outlined column specs

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  through: {
+    
+  }
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  
+})
 
 module.exports = {
   Product,
