d1aef0a5b51141698a1c320a887a82fd8e536443
Hannah Lee <013hannah.a.lee@gmail.com>
2021-05-04 19:58:03 -0500

    added associations for the models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -6,24 +6,23 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  through: {
-    
-  }
+  foreignKey: 'category_id',
 })
 
 // Categories have many Products
 Category.hasMany(Product, {
-
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
 })
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-
+  foreignKey: 'tag_id',
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  
+  foreignKey: 'tag_id',
 })
 
 module.exports = {
