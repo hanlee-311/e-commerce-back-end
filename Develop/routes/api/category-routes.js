6ab2c72e8eb9d17e89b9a669344a3ea950bc6c3d
Hannah Lee <013hannah.a.lee@gmail.com>
2021-05-04 20:09:19 -0500

    fixed add employee to include a manager

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,6 +6,11 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const 
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
