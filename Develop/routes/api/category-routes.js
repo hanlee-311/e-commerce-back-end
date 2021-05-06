be1e42d2ef0eab8e7269e9406ead26de7b7a2db7
Hannah Lee <013hannah.a.lee@gmail.com>
2021-05-06 16:35:49 -0500

    updated api routes for category

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -31,18 +31,40 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
   // create a new category
   Category.create(req.body).then((data) => {
-    res.status(200).json(data).catch((err) => {
+    res.status(200).json(data)
+  })
+    .catch((err) => {
       res.status(500).json(err);
     })
-  })
 });
+
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    }
+  }).then((data) => {
+    res.status(200).json(data)
+  })
+    .catch((err) => {
+      res.status(500).json(err);
+    })
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    }
+  }).then((data) => {
+    res.status(200).json(data)
+  })
+    .catch((err) => {
+      res.status(500).json(err);
+    })
 });
 
 module.exports = router;
