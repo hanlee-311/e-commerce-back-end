2778ef115bd2a39f2d54c1a066ca3be31815d3e9
Hannah Lee <013hannah.a.lee@gmail.com>
2021-05-06 16:20:48 -0500

    fixed models and seeded the tables

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,20 +6,35 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
-  try {
-    const 
-  } catch (err) {
+  Category.findAll({
+    include: [Product],
+  }).then((data) => {
+    res.json(data);
+  }).catch((err) => {
     res.status(500).json(err);
-  }
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    include: [Product],
+    where: { id: req.params.id },
+  }).then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.status(500).json(err);
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body).then((data) => {
+    res.status(200).json(data).catch((err) => {
+      res.status(500).json(err);
+    })
+  })
 });
 
 router.put('/:id', (req, res) => {
